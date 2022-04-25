fdb94f38813f780147315fee7f50156273c43c71
gwangbaekun <gwangbaekun@gmail.com>
2022-04-25 12:16:18 +0900

    Coin List with typescript & react-router-dom

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -67,14 +67,15 @@ function Coins() {
 
   useEffect(() => {
     (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
+      const response = await (
+        await fetch("https://api.coinpaprika.com/v1/coins")
+      ).json();
+      setCoins(response.slice(0, 100));
       setLoading(false);
     })();
   }, []);
   // 요부분 작성해야할듯
-  // ()?
+  // ()? // 비동기 함수를 음 동기로 바꾸는 방법
   // const response = await fetch("https://api.coinpaprika.com/v1/coins").json() 은 왜 안됨?
 
   return (
