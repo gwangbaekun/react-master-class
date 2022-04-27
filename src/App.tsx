837ff4dd16abb274db787f9bbe540f75577307e4
gwangbaekun <gwangbaekun@gmail.com>
2022-04-27 17:15:38 +0900

    Variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { useEffect } from "react";
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import AddBoard from "./components/AddBoard";
-import Board from "./components/Board";
-import Trash from "./components/Trash";
-import { getLocalToDos, setLocalToDos } from "./recoilPersist";
-import { toDoState } from "./state";
+import { motion } from "framer-motion";
 
 export const Wrapper = styled.div`
   display: flex;
@@ -23,93 +11,30 @@ export const Wrapper = styled.div`
   height: 100vh;
 `;
 
-const Boards = styled.div`
-  display: grid;
-  width: 100%;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: {
+    scale: 0,
+  },
+  end: {
+    scale: 1,
+    rotateZ: 360,
+    transition: { type: "spring", duration: 0.8 },
+  },
+};
+
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
-    if (!destination) return;
-    if (destination?.droppableId === "trash") {
-      setToDos((prev) => {
-        const trashToDos = [...prev[source.droppableId]];
-        const erasedToDos = trashToDos.filter(
-          (toDo) => toDo.id !== +draggableId
-        );
-        return {
-          ...prev,
-          [source.droppableId]: erasedToDos,
-        };
-      });
-      return;
-    }
-    if (destination?.droppableId === source.droppableId) {
-      setToDos((prev) => {
-        const copyToDos = [...prev[source.droppableId]];
-        const taskObj = copyToDos[source.index];
-        copyToDos.splice(source.index, 1);
-        copyToDos.splice(destination?.index, 0, taskObj);
-        return {
-          ...prev,
-          [source.droppableId]: copyToDos,
-        };
-      });
-    }
-    if (destination?.droppableId !== source.droppableId) {
-      setToDos((prev) => {
-        const copyToDosBefore = [...prev[source.droppableId]];
-        const copyToDosAfter = [...prev[destination?.droppableId]];
-        const taskObj = copyToDosBefore[source.index];
-
-        copyToDosBefore.splice(source.index, 1);
-        copyToDosAfter.splice(destination?.index, 0, taskObj);
-        return {
-          ...prev,
-          [source.droppableId]: copyToDosBefore,
-          [destination?.droppableId]: copyToDosAfter,
-        };
-      });
-    }
-  };
-
-  useEffect(() => {
-    setToDos(getLocalToDos());
-  }, []);
-
-  useEffect(() => {
-    setLocalToDos(toDos);
-  }, [toDos]);
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <AddBoard />
-      <Droppable
-        droppableId={"category"}
-        type={"category"}
-        direction={"horizontal"}
-      >
-        {(p) => (
-          <Wrapper ref={p.innerRef} {...p.droppableProps}>
-            <Boards>
-              {Object.keys(toDos).map((boardId, index) => (
-                <Board
-                  key={`${boardId}`}
-                  index={index}
-                  boardId={boardId}
-                  toDos={toDos[boardId]}
-                />
-              ))}
-            </Boards>
-            <Trash />
-          </Wrapper>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <Wrapper>
+      <Box variants={myVars} initial="start" animate="end" />
+    </Wrapper>
   );
 }
 
--- src/Theme.ts
+++ src/Theme.ts
@@ -2,73 +2,69 @@ import { DefaultTheme } from "styled-components";
 
 import { createGlobalStyle } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#3F8CF2",
-  boardColor: "#DADFE9",
-  cardColor: "white",
-};
+export const darkTheme: DefaultTheme = {};
 
 export const GlobalStyle = createGlobalStyle`
-  /* http://meyerweb.com/eric/tools/css/reset/ 
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-@import url('https://fonts.googleapis.com/css2?family=Montagu+Slab:wght@100&family=Source+Sans+3:wght@200&family=Source+Sans+Pro:wght@300;400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
 del, dfn, em, img, ins, kbd, q, s, samp,
 small, strike, strong, sub, sup, tt, var,
 b, u, i, center,
-dl, dt, dd, ol, ul, li,
+dl, dt, dd, menu, ol, ul, li,
 fieldset, form, label, legend,
 table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
 time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
 }
 body {
-	line-height: 1;
+  line-height: 1;
 }
-ol, ul {
-	list-style: none;
+menu, ol, ul {
+  list-style: none;
 }
 blockquote, q {
-	quotes: none;
+  quotes: none;
 }
 blockquote:before, blockquote:after,
 q:before, q:after {
-	content: '';
-	content: none;
+  content: '';
+  content: none;
 }
 table {
-	border-collapse: collapse;
-	border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 * {
   box-sizing: border-box;
 }
 body {
-	font-weight: 300;
+  font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
   color:black;
   line-height: 1.2;
+  background:linear-gradient(135deg,#e09,#d0e);
 }
 a {
-  text-decoration: none;
+  text-decoration:none;
   color:inherit;
 }
 `;
