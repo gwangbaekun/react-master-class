5abbb738e4e151cea02437dd5842d1742d83da06
gwangbaekun <gwangbaekun@gmail.com>
2022-04-27 17:37:34 +0900

    Gestures

--- src/App.tsx
+++ src/App.tsx
@@ -16,71 +16,30 @@ const Box = styled(motion.div)`
   height: 200px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: white;
   border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  border-radius: 35px;
-`;
-
 const boxVariants = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.3,
-    },
+  hover: {
+    scale: 2,
+    rotateZ: 90,
   },
-};
-
-const circleVarients = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      type: "spring",
-      bounce: 0.8,
-      duration: 3,
-    },
-  },
-};
-
-const myVars = {
-  start: {
-    scale: 0,
-  },
-  end: {
-    scale: 1,
-    rotateZ: 360,
-    transition: { type: "spring", duration: 5 },
+  onclick: {
+    scale: 1.5,
+    borderRadius: "100px",
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVarients} />
-        <Circle variants={circleVarients} />
-        <Circle variants={circleVarients} />
-        <Circle variants={circleVarients} />
-      </Box>
+      <Box
+        variants={boxVariants}
+        whileHover={boxVariants.hover}
+        whileTap={boxVariants.onclick}
+      />
     </Wrapper>
   );
 }
